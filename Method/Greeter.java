29d0c5a77c74f00cc7ea19f195584cada4de4444
Tuyen Dang <tuyendq@gmail.com>
2025-11-04 20:10:37 +0700

    update Greeter.java class to use new greeting format

--- Method/Greeter.java
+++ Method/Greeter.java
@@ -4,8 +4,17 @@ class Greeter {
     System.out.println("Nice day to you !");
   }
 
+  // Give the correct type to the argument of the method
+  static void specificGreeter(String name) {
+    // Call the correct variable given as an argument to the method
+    System.out.println("Nice day to you, " + name + "!");
+  }  
+
   public static void main(String[] args) {
     // Call the above function to have the program greet you
     genericGreeter();
+
+    String firstName = "Caleb";
+    specificGreeter(firstName);    
   }
 }
