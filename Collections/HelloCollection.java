0246f549483d0bb422b1ce3be77e55cf765e3211
Tuyen Dang <tuyendq@gmail.com>
2025-11-08 20:43:06 +0700

    add HelloCollection.java to Collection folder

---
+++ Collections/HelloCollection.java
@@ -0,0 +1,14 @@
+// Import all types from Collections Framework package
+import java.util.*;
+
+public class HelloCollection {
+
+	public static void main(String[] args) {
+		// Create a variable for a group of Strings
+		ArrayList<String> list;
+        // Set the variable to new ArrayList
+        list = new ArrayList<String>();
+		list.add("hello");
+		System.out.println(list);
+	}
+}
