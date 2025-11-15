9979d6c03ec527f95e8e9c028f2351ba7f04bebb
Tuyen Dang <tuyendq@gmail.com>
2025-11-15 19:56:26 +0700

    update NobelForPhysicsLaureates.java

--- Array/NobelForPhysicsLaureates.java
+++ Array/NobelForPhysicsLaureates.java
@@ -6,5 +6,10 @@ class NobelForPhysicsLaureates {
     
     	// Assign values to years
         int[] years = {1903, 1921, 1938};
+
+        System.out.println("The Nobel Prize winners in Physics are:");
+        for (int i = 0; i < nobelLaureates.length; i++) {
+            System.out.println(nobelLaureates[i] + ", " + years[i]);
+        }
 	}
 } 
